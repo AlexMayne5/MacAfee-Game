0bb975e37646a38f722c4600c85945904612290c
fluxcabury <mark.williamson02@outlook.com>
2019-12-05 13:17:55 +0000

    Can check if grounded
    thank you internet

--- MacAfee Game - Unity/Assets/Scripts/PlayerController.cs
+++ MacAfee Game - Unity/Assets/Scripts/PlayerController.cs
@@ -9,35 +9,71 @@ public class PlayerController : MonoBehaviour
     public LayerMask whatIsGround;
     private bool grounded;
     public Collider2D PlayerCollider;
+    public float distToGround;
     // Use this for initialization
     void Start()
     {
         Debug.Log("Hola");
+        distToGround = PlayerCollider.bounds.extents.y;
     }
     void fixedUpdate()
     {
-        //grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
     }
     // Update is called once per frame
 
-    void OnCollisionEnter(Collision collision)
+    /*
+        void OnCollisionStay(Collision collision)
+        {
+            Debug.Log("Touching a thing");
+            if (collision.gameObject.tag == "ground")
+            {
+                grounded = true;
+                Debug.Log("touching ground");
+            }
+        }
+
+        public bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+        }
+        */
+    void OnCollisionStay2D(Collision2D collider)
     {
-        Debug.Log("Touching a thing");
-        if (collision.gameObject.tag == "ground")
+        CheckIfGrounded();
+    }
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        grounded = false;
+    }
+
+    private void CheckIfGrounded()
+    {
+        RaycastHit2D[] hits;
+
+        //We raycast down 1 pixel from this position to check for a collider
+        Vector2 positionToCheck = transform.position;
+        hits = Physics2D.RaycastAll(positionToCheck, new Vector2(0, -1), 0.01f);
+
+        //if a collider was hit, we are grounded
+        if (hits.Length > 0)
         {
             grounded = true;
-            Debug.Log("touching ground");
         }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        
+        if (Input.GetKeyDown(KeyCode.W) && grounded == true)
         {
+            Debug.Log("jump");
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
         }
         if (Input.GetKey(KeyCode.D))
         {
+            Debug.Log(grounded);
             if (grounded == true)
             {
                 GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);

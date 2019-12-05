5b98a46a4688a72282485313dad4ceb846a7cb41
fluxcabury <mark.williamson02@outlook.com>
2019-12-05 12:36:57 +0000

    Camra followes player and mod player movement

--- MacAfee Game - Unity/Assets/Scripts/PlayerController.cs
+++ MacAfee Game - Unity/Assets/Scripts/PlayerController.cs
@@ -8,15 +8,28 @@ public class PlayerController : MonoBehaviour
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool grounded;
+    public Collider2D PlayerCollider;
     // Use this for initialization
     void Start()
     {
+        Debug.Log("Hola");
     }
     void fixedUpdate()
     {
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        //grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
     // Update is called once per frame
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Touching a thing");
+        if (collision.gameObject.tag == "ground")
+        {
+            grounded = true;
+            Debug.Log("touching ground");
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
@@ -25,11 +38,27 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if (grounded == true)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            } else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed/5, GetComponent<Rigidbody2D>().velocity.y);
+            }
+            
+
         }
         if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if (grounded == true)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed / 5, GetComponent<Rigidbody2D>().velocity.y);
+            }
         }
+        grounded = false;
     }
 }

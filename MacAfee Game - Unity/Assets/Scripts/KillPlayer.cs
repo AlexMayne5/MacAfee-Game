8d07f82492b8201b0927b040e27c601b0d805fc7
fluxcabury <mark.williamson02@outlook.com>
2020-01-21 22:57:25 +0000

    Player Death

---
+++ MacAfee Game - Unity/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour{
+    [SerializeField]Transform Spawn;
+
+  void OnCollisionEnter2D(Collision2D col){
+    if(col.transform.CompareTag("Player"))
+      col.transform.position = Spawn.position;
+  }
+}
